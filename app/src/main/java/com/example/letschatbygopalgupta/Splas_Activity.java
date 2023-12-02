e09de08e0b2c685dad30dadfbe13312dbe122ea6
Gopal Gupta <k7523337@gmail.com>
2023-12-02 23:12:16 +0530

    Learning Firebase By making chatapp

---
+++ app/src/main/java/com/example/letschatbygopalgupta/Splas_Activity.java
@@ -0,0 +1,27 @@
+package com.example.letschatbygopalgupta;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class Splas_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splas);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splas_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
---
+++ app/src/main/java/com/example/letschatbygopalgupta/signup.java
@@ -0,0 +1,167 @@
+package com.example.letschatbygopalgupta;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class signup extends AppCompatActivity {
+
+    TextView login;
+    EditText Username,Email,pass,repass;
+    Button signupbtn;
+    ImageView img;
+    FirebaseAuth auth;
+    Uri imageuri;
+    String imageUri;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        login = findViewById(R.id.login);
+        Username = findViewById(R.id.username);
+        Email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        repass = findViewById(R.id.repassword);
+        img = findViewById(R.id.profiledp);
+        signupbtn = findViewById(R.id.signupbutton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signup.this,login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String namee = Username.getText().toString();
+                String emaill = Email.getText().toString();
+                String passs = pass.getText().toString();
+                String repasss = repass.getText().toString();
+                String statuss = "Hey I'm Using This Application";
+
+                if(TextUtils.isEmpty(namee) || TextUtils.isEmpty(namee) || TextUtils.isEmpty(passs) || TextUtils.isEmpty(repasss)){
+                    Toast.makeText(signup.this, "Please Enter Valid Information", Toast.LENGTH_SHORT).show();
+                }
+                else if(!emaill.matches(emailPattern)){
+                    Email.setError("Type a valid email");
+                } else if (passs.length()<6) {
+                    pass.setError("password must be greater than or equals to six");
+                } else if (!passs.equals(repasss)) {
+                    pass.setError("password doesn't match");
+                }
+                else{
+                    auth.createUserWithEmailAndPassword(emaill,passs).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                String id = task.getResult().getUser().getUid();
+                                DatabaseReference reference = database.getReference().child("user").child(id);
+                                StorageReference storageReference = storage.getReference().child("upload").child(id);
+
+                                if(imageuri!=null){
+                                    storageReference.putFile(imageuri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                            if(task.isSuccessful()){
+                                                storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                    @Override
+                                                    public void onSuccess(Uri uri) {
+                                                        imageUri = uri.toString();
+                                                        Users users = new Users(id,namee,emaill,passs,repasss,imageUri,statuss);
+                                                        reference.setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if(task.isSuccessful()){
+                                                                    Intent intent = new Intent(signup.this,MainActivity.class);
+                                                                    startActivity(intent);
+                                                                    finish();
+                                                                }
+                                                                else {
+                                                                    Toast.makeText(signup.this, "error in creating your account", Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
+                                }else {
+                                    String statuss = "Hey I'm Using This Application";
+                                    imageUri = "https://firebasestorage.googleapis.com/v0/b/let-s-chat-bce19.appspot.com/o/loginuser.png?alt=media&token=49c567fe-61bf-439b-ae8b-de976c90b042";
+                                    Users user = new Users(id,namee,emaill,passs,repasss,imageUri,statuss);
+                                    reference.setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Intent intent = new Intent(signup.this,MainActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                            else {
+                                                Toast.makeText(signup.this, "error in creating your account", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),10);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==10){
+            if(data!=null){
+                imageuri = data.getData();
+                img.setImageURI(imageuri);
+            }
+        }
+    }
+}
