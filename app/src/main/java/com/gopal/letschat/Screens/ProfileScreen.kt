51d899d150e6649d87084435957fd88ee7e0f3ce
Gopal Gupta <k7523337@gmail.com>
2024-10-14 21:56:23 +0530

    Let's Chat

--- app/src/main/java/com/gopal/letschat/Screens/ProfileScreen.kt
+++ app/src/main/java/com/gopal/letschat/Screens/ProfileScreen.kt
@@ -1,7 +1,9 @@
 package com.gopal.letschat.Screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun ProfileScreen() {
+    Text(text = "profile screen")
 }
--- app/src/main/java/com/gopal/letschat/Screens/StatusScreen.kt
+++ app/src/main/java/com/gopal/letschat/Screens/StatusScreen.kt
@@ -1,7 +1,9 @@
 package com.gopal.letschat.Screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun StatusScreen() {
+    Text(text = "Status screen")
 }
