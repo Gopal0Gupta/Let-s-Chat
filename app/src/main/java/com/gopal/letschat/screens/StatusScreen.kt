d6f22a00791283322ecac3093df0abe7799e87e4
Gopal Gupta <k7523337@gmail.com>
2025-06-09 00:04:49 +0530

    working on Status Screen

--- app/src/main/java/com/gopal/letschat/screens/StatusScreen.kt
+++ app/src/main/java/com/gopal/letschat/screens/StatusScreen.kt
@@ -1,20 +1,68 @@
 package com.gopal.letschat.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.gopal.letschat.LCViewModel
+import com.gopal.letschat.TitleText
+import com.gopal.letschat.commonProgressBar
 
 @Composable
 fun StatusScreen(navController: NavController, vm: LCViewModel) {
-    Text(text = "Status screen")
-    BottomNavigationMenu(BottomNavigationItem.STATUSLIST, navController)
+    val inProcess = vm.inProgressStatus
+    if (inProcess) {
+        commonProgressBar()
+    } else {
+        val statuses = vm.status
+        val userData = vm.userdata
+        Scaffold(
+            floatingActionButton = {
+                FAB {
+
+                }
+            },
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it)
+                ) {
+                    TitleText(txt = "Status")
+                    if (statuses.isEmpty()) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(text = "No Statuses Available")
+                        }
+                    }else{
+
+                    }
+                }
+            }
+        )
+        Text(text = "Status screen")
+        BottomNavigationMenu(BottomNavigationItem.STATUSLIST, navController)
+    }
 }
 
 @Composable
@@ -25,6 +73,10 @@ fun FAB(onFabClick: () -> Unit) {
         shape = CircleShape,
         modifier = Modifier.padding(bottom = 40.dp)
     ) {
-
+        Icon(
+            imageVector = Icons.Rounded.Edit,
+            contentDescription = "Add Status",
+            tint = Color.White
+        )
     }
 }
